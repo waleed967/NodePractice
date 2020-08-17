24c49567a57fb1265a6ae8936ada09976c0d4f56
waleed967 <waleedqureshi90@yahoo.com>
2020-08-17 03:03:05 -0700

    A&D Order

---
+++ Ascending&Descending/Escape.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "QWERTYuiop.7",
+    database: "myEmployee"
+});
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM reporters ORDER BY id DESC", function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
