de672ec0ced63424997e6f81a2917121deafebdd
waleed967 <waleedqureshi90@yahoo.com>
2020-08-19 00:19:26 -0700

    FindRecords

---
+++ RecordFinding/FindAll.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("myDepartment");
+
+
+    dbo.collection("students").find({}).toArray(function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        db.close();
+    });
+});
